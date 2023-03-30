f32e1a4b93916dc887d148f4b7d66787dec0f829
Roger Medellin <medr33@yahoo.com>
2023-03-30 12:00:00 -0400

    add timer

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,6 @@
 var startButton = document.querySelector(".start-button")
 var timer;
-
+var timerCount=80
 
 //questions
 
@@ -12,42 +12,33 @@ function displayMessage(type, message) {
   }
 
 
+ //Event Listener that utilze the first function 
 
+// start time function with a timer and go to next function, 
 
-  
-//The startGame function is called when the start button is clicked
-function startGame(){
-    isWin = false;
-    timerCount = 80;
+function startQuiz(){
+  timer = setInterval(clock,1000)
+
 //Prevents start button from being clicked when round is in progress
     startButton.disabled = true;
-    startTimer()
-}
+    // function two callout
+  }
 
-
-// The setTimer function starts and stops the timer and triggers winGame() and loseGame()
-function startTimer(){
+function clock(){
  //Set timer
- timer = setInterval(function(){
+ 
    timerCount--;
    timerElement.textContent = timerCount;
-   if(timerCount >= 0){
-  // Tests if win condition is met
-      if (isWin && timerCount > 0) {
-    // Clears interval and stops timer
-       clearInterval(timer);
-        winGame();
-    }
- }
+   
+  
 // Tests if time has run out
   if (timerCount === 0) {
-  // Clears interval
-   clearInterval(timer);
+
    loseGame();
    }  
-  }, 1000);
+  
 }
 
 
 
-startButton.addEventListener("click",startGame);
+startButton.addEventListener("click",startQuiz);
