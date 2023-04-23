5b8e1b4e30b3ea41fbf90564086d83b457546de4
Roger Medellin <medr33@yahoo.com>
2023-04-23 03:48:14 -0400

    clean up questions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,68 +3,113 @@ var timer;
 var timerCount=80
 
 //Questions
+var nextBtn =document.querySelector("#next-btn")
+var questionEl = document.querrySelector("#questions");
+var answerBtn = document.querySelectorAll(".option-btn");
+var optionBtnA = document.querrySelector("#a");
+var optionBtnB = document.querrySelector("#b");
+var optionBtnC = document.querrySelector("#c");
+var optionBtnD = document.querrySelector("#d");
+var questionEl = document.getElementById("quest");
+var randomQuestons = [
+  
+  {
+    question: "Java script handles?"
+    options; [
+      "Content",
+      "format",
+      "Design",
+      "overall functionality of the application",
+     ],
+
+    correctAnswer: "overall functionality of the application" 
+  },
+  {
+    question: "Java script handles?"
+    options: [
+      "Content",
+      "format",
+      "Design",
+      "overall functionality of the application",
+    ],
+    correctAnswer: "overall functionality of the application" 
+  },
+  {
+    question: "Java script handles?"
+    options: [
+      "Content",
+      "format",
+      "Design",
+      "overall functionality of the application",
+    ],
+    correctAnswer: "overall functionality of the application" 
+  },
+]
+
+
+
 
 //var body  document.body;
 
-var quizQuestionEl = document.createElement('div')
+// var quizQuestionEl = document.createElement('div')
 
-var questionsEl = document.createElement("ol");
-var li1 =document.createElement("li");
-var li2 =document.createElement("li");
-var li3 =document.createElement("li");
-var li4 =document.createElement("li");
+// var questionsEl = document.createElement("ol");
+// var li1 =document.createElement("li");
+// var li2 =document.createElement("li");
+// var li3 =document.createElement("li");
+// var li4 =document.createElement("li");
 
-li1.textContent = "Java script handles?";
-li2.textContent = "If var a = 10 and var b = 10, what is the Value and type?";
-li3.textContent = "The keyword this refers to?";
-li4.textContent = 'What does "i++" stand for in a loop statement?';
+// li1.textContent = "Java script handles?";
+// li2.textContent = "If var a = 10 and var b = 10, what is the Value and type?";
+// li3.textContent = "The keyword this refers to?";
+// li4.textContent = 'What does "i++" stand for in a loop statement?';
 
-questionsEl.appendChild(li1);
-questionsEl.appendChild(li2);
-questionsEl.appendChild(li3);
-questionsEl.appendChild(li4);
+// questionsEl.appendChild(li1);
+// questionsEl.appendChild(li2);
+// questionsEl.appendChild(li3);
+// questionsEl.appendChild(li4);
 
-quizQuestionEl.appendChild(questionsEl);
+// quizQuestionEl.appendChild(questionsEl);
 
-quizQuestionEl.textContent = "Quiz"
-console.log("quizQestionEl")
-
-
-renderLastRegistered();
-
-function displayMessage(type, message) {
-    msgDiv.textContent = message;
-    msgDiv.setAttribute("class", type);
-  }
+// quizQuestionEl.textContent = "Quiz"
+// console.log("quizQestionEl")
 
 
- //Event Listener that utilze the first function 
+// renderLastRegistered();
 
-// start time function with a timer and go to next function, 
+// function displayMessage(type, message) {
+//     msgDiv.textContent = message;
+//     msgDiv.setAttribute("class", type);
+//   }
 
-function startButton(){
-  timer = setInterval(clock,1000)
 
-//Prevents start button from being clicked when round is in progress
-    startButton.disabled = true;
-    // function two callout
-  }
+//  //Event Listener that utilze the first function 
 
-function clock(){
- //Set timer
+// // start time function with a timer and go to next function, 
+
+// function startButton(){
+//   timer = setInterval(clock,1000)
+
+// //Prevents start button from being clicked when round is in progress
+//     startButton.disabled = true;
+//     // function two callout
+//   }
+
+// function clock(){
+//  //Set timer
  
-   timerCount--;
-   timerElement.textContent = timerCount;
+//    timerCount--;
+//    timerElement.textContent = timerCount;
    
   
-// Tests if time has run out
-  if (timerCount === 0) {
+// // Tests if time has run out
+//   if (timerCount === 0) {
 
-   loseGame();
-   }  
+//    loseGame();
+//    }  
   
-}
+// }
 
 
 
-startButton.addEventListener("click",startQuiz);
+// startButton.addEventListener("click",startQuiz);
